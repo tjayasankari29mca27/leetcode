ebc0fb58cd1549ad8b56606cf2a1b5ba738b403a
tjayasankari29mca27 <tjayasankari29_mca27@mepcoeng.ac.in>
2026-05-11 20:23:43 +0530

    Create searchinrotatedarrayII.java

---
+++ searchinrotatedarrayII.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            // The critical addition for duplicates:
+            // If endpoints and mid are the same, we can't tell which side is sorted.
+            // Just shrink the window and try again.
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } 
+            // Left half is sorted
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } 
+            // Right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
