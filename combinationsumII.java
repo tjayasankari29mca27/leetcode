a14ec7eab8bd8d74b558916b2577e09c002ed6a2
tjayasankari29mca27 <tjayasankari29_mca27@mepcoeng.ac.in>
2026-05-11 12:30:48 +0530

    Create combinationsumII.java

---
+++ combinationsumII.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, new ArrayList<>(), results);
+        return results;
+    }
+    private void backtrack(int[] candidates, int remain, int start, List<Integer> current, List<List<Integer>> results) {
+        if (remain == 0) {
+            results.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > remain) break;
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            current.add(candidates[i]);
+            backtrack(candidates, remain - candidates[i], i + 1, current, results);
+            current.remove(current.size() - 1);
+        }
+    }
+}
