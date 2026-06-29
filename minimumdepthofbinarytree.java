042a632753a13888dfe966c4003745eb1a20b5be
tjayasankari29mca27 <tjayasankari29_mca27@mepcoeng.ac.in>
2026-06-29 14:20:27 +0530

    Create minimumdepthofbinarytree.java

---
+++ minimumdepthofbinarytree.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+class Solution {
+    public int minDepth(TreeNode root) {
+        // Base case: an empty tree has a depth of 0
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if (currentNode.left == null && currentNode.right == null) {
+                    return depth;
+                }
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
